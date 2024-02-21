a33a8967526986db7ed8e464a7ff74468d0e86bc
stayprimitive <119599068+stayprimitive@users.noreply.github.com>
2024-02-22 00:12:03 +0200

    task-1 - DONE!

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,15 @@
-function makeTransaction(quantity, pricePerDroid) {
-    let totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+function slugify(title) {
+    let result = "";
+    for (let i = 0; i < title.length; i++){
+        if (title[i] === " ") {
+            result += "-";
+        } else {
+            result += title[i]
+        }
+    }
+    return result.toLowerCase();
 }
-console.log(makeTransaction(5, 3000)); 
-console.log(makeTransaction(3, 1000)); 
-console.log(makeTransaction(10, 500));
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
