54bfbaccab1ed824ecddd344fe29a4267ad98788
stayprimitive <119599068+stayprimitive@users.noreply.github.com>
2024-02-22 00:31:20 +0200

    task-3 - DONE!

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,14 @@
-function getElementWidth(content, padding, border) {
-    content = Number.parseFloat(content);
-    padding = Number.parseFloat(padding);
-    border = Number.parseFloat(border);
-    return content + padding * 2 + border * 2;
+function filterArray(numbers, value) {
+    let result = [];
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers[i] > value) {
+            result.push(numbers[i])
+        }
+    }
+    return result;
 }
-
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
